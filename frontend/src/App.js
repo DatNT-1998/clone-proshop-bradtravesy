49fc40dbb1abd2c53564e56144a547c4f699e5b8
DatNT2-F1-D1 <datnt2@fabbi.io>
2020-12-30 10:49:05 +0700

    Add ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+;
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './components/screens/HomeScreen';
+import ProductScreen from './components/screens/ProductScreen';
+
 
 function App() {
   return (
-    <>
-      <h1> Welcome To ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container className="py-3">
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
