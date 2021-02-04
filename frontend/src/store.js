0c8fc9bbcc681c76c73ce86f817a543741ecf8f5
DatNT2-F1-D1 <datnt2@fabbi.io>
2021-02-04 21:47:04 +0700

    redux: add user login

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,20 +2,26 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js';
-import {cartReducer} from './reducers/cartReducers';
+import { cartReducer } from './reducers/cartReducers';
+import { userInfoReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userInfo: userInfoReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
-const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
-};
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
 
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
+
+};
+ 
 const middleware = [thunk];
 
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
