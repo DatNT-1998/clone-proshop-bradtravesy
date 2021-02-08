0df71c2479a40b2660f951173672d8515dd5b8e8
DatNT2-F1-D1 <datnt2@fabbi.io>
2021-02-09 00:10:57 +0700

    add feature logout and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Footer from './components/Footer'
 import HomeScreen from './components/screens/HomeScreen';
 import ProductScreen from './components/screens/ProductScreen';
 import CartScreen from './components/screens/CartScreen';
+import LoginScreen from './components/screens/LoginScreen';
+import RegisterScreen from './components/screens/RegisterScreen';
 
 
 function App() {
@@ -15,9 +17,11 @@ function App() {
       <Header />
       <main>
         <Container className="py-3">
-          <Route path="/" component={HomeScreen} exact />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
